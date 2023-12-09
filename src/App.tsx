8d1b9cb44ff1e49db4193f5486b5fd9c255f8fb2
Luis Suarez <suarezluis@gmail.com>
2023-12-09 13:01:04 -0600

    Add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import {
   Tag,
   useClipboard,
 } from "@chakra-ui/react";
-import { link } from "fs";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -37,6 +36,16 @@ function App() {
   const [bcc, setBcc] = useState("");
   const [subject, setSubject] = useState("");
   const [body, setBody] = useState("");
+
+  const clearForm = (): void => {
+    setTo("");
+    setCc("");
+    setBcc("");
+    setSubject("");
+    setBody("");
+    setLinkValue("");
+    setHtmlValue("");
+  };
 
   const generateLink = (): void => {
     let link = `mailto:${to.replaceAll(" ", "")}?`;
@@ -60,110 +69,155 @@ function App() {
     setHtmlValue(`<a href="${link}">Send me an email</a>`);
   };
   return (
-    <Box className="App" width="100vw" height="100vh" padding="4">
-      <Flex justifyContent="space-between">
-        <Box>Buy me coffee</Box>
-        <Heading>Mailto Link Generator</Heading>
-        <Box>
+    <Flex
+      className="App"
+      width="100vw"
+      height="100vh"
+      padding="4"
+      justifyContent="center"
+    >
+      <Box maxW="800px">
+        <Flex justifyContent="space-between">
+          <Button
+            colorScheme="yellow"
+            onClick={() => {
+              // open the link in a new tab
+              window.open("https://www.buymeacoffee.com/suarezluis");
+            }}
+          >
+            Buy me coffee ☕
+          </Button>
+
           <Button onClick={toggleColorMode}>
             Toggle {colorMode === "light" ? "Dark" : "Light"}
           </Button>
-        </Box>
-      </Flex>
-      <Flex flexDirection="column" paddingX="40" paddingY="10">
-        <Text as="p" marginY="4">
-          This is a simple tool to generate a mailto link for you to use in your
-          HTML code. Mailto links are used to open the user's default email
-          client and pre-populate the email with the information you provide in
-          the link.
-        </Text>
-
-        <Text as="p" marginY="4">
-          Fill out the form below and click generate to create a mailto link.
-        </Text>
-        <InputGroup mb="4">
-          <InputLeftAddon width="60px" children="to:" />
-          <Input
-            type="email"
-            placeholder="comma separated email addresses (required)"
-            value={to}
-            onChange={(e) => setTo(e.currentTarget.value)}
-          />
-        </InputGroup>
-        <InputGroup mb="4">
-          <InputLeftAddon width="60px" children="cc:" />
-          <Input
-            type="text"
-            placeholder="comma separated email addresses (optional)"
-            value={cc}
-            onChange={(e) => setCc(e.currentTarget.value)}
-          />
-        </InputGroup>
-        <InputGroup mb="4">
-          <InputLeftAddon width="60px" children="bcc:" />
-          <Input
-            type="text"
-            placeholder="comma separated email addresses (optional)"
-            value={bcc}
-            onChange={(e) => setBcc(e.currentTarget.value)}
-          />
-        </InputGroup>
-        <FormLabel>Subject:</FormLabel>
-        <Input
-          placeholder="subject line (optional)"
-          value={subject}
-          onChange={(e) => setSubject(e.currentTarget.value)}
-        />
-        <FormLabel mt="4">Body:</FormLabel>
-        <Textarea
-          placeholder="email body (optional)"
-          value={body}
-          onChange={(e) => setBody(e.currentTarget.value)}
-        />
-      </Flex>
-      <Box paddingX="10">
-        <Flex justifyContent="space-evenly">
-          <Button>Clear</Button>
-          <Button onClick={generateLink}>Generate</Button>
         </Flex>
+        <Flex justifyContent="center">
+          <Heading as="h1" size="xl" marginY="4">
+            Mailto Link Generator
+          </Heading>
+        </Flex>
+        <Flex flexDirection="column" paddingY="5">
+          <Text as="p" marginY="4">
+            This is a simple tool to generate a mailto link for you to use in
+            your HTML code. Mailto links are used to open the user's default
+            email client and pre-populate the email with the information you
+            provide in the link.
+          </Text>
 
-        {linkValue && (
-          <Flex mb="4" alignItems="center" flexDirection="column">
-            <Box>
-              <Button
-                onClick={() => {
-                  if (linkValue) {
-                    window.location = linkValue as unknown as Location;
-                  }
-                }}
-              >
-                Test your link
-              </Button>
-            </Box>
-            <Flex flexDirection="column" maxWidth="75%">
-              <Flex justifyContent="space-between" paddingY="4">
-                <Tag>Link</Tag>
-                <Button size="xs" onClick={onLinkCopy}>
-                  {linkHasCopied ? "Copied!" : "Copy"}
-                </Button>
-              </Flex>
-              <Code padding="4" variant="subtle" colorScheme="green">
-                {linkValue}
-              </Code>
-              <Flex justifyContent="space-between" paddingY="4">
-                <Tag>HTML</Tag>
-                <Button size="xs" onClick={onHtmlCopy}>
-                  {htmlHasCopied ? "Copied!" : "Copy"}
-                </Button>
-              </Flex>
-              <Code padding="4" variant="subtle" colorScheme="blue">
-                {HtmlValue}
-              </Code>
-            </Flex>
+          <Text as="p" marginY="4">
+            Fill out the form below and click generate to create a mailto link.
+          </Text>
+          <InputGroup mb="4">
+            <InputLeftAddon width="60px" children="to:" />
+            <Input
+              type="email"
+              placeholder="comma separated email addresses (required)"
+              value={to}
+              onChange={(e) => setTo(e.currentTarget.value)}
+            />
+          </InputGroup>
+          <InputGroup mb="4">
+            <InputLeftAddon width="60px" children="cc:" />
+            <Input
+              type="text"
+              placeholder="comma separated email addresses (optional)"
+              value={cc}
+              onChange={(e) => setCc(e.currentTarget.value)}
+            />
+          </InputGroup>
+          <InputGroup mb="4">
+            <InputLeftAddon width="60px" children="bcc:" />
+            <Input
+              type="text"
+              placeholder="comma separated email addresses (optional)"
+              value={bcc}
+              onChange={(e) => setBcc(e.currentTarget.value)}
+            />
+          </InputGroup>
+          <FormLabel>Subject:</FormLabel>
+          <Input
+            placeholder="subject line (optional)"
+            value={subject}
+            onChange={(e) => setSubject(e.currentTarget.value)}
+          />
+          <FormLabel mt="4">Body:</FormLabel>
+          <Textarea
+            placeholder="email body (optional)"
+            value={body}
+            onChange={(e) => setBody(e.currentTarget.value)}
+          />
+        </Flex>
+        <Box paddingX="10">
+          <Flex justifyContent="space-evenly">
+            <Button onClick={clearForm}>Clear</Button>
+            <Button onClick={generateLink}>Generate</Button>
           </Flex>
-        )}
+
+          {linkValue && (
+            <Flex mb="4" alignItems="center" flexDirection="column">
+              <Box>
+                <Button
+                  onClick={() => {
+                    if (linkValue) {
+                      window.location = linkValue as unknown as Location;
+                    }
+                  }}
+                >
+                  Test your link
+                </Button>
+              </Box>
+              <Flex flexDirection="column" maxWidth="75%">
+                <Flex justifyContent="space-between" paddingY="4">
+                  <Tag>Link</Tag>
+                  <Button size="xs" onClick={onLinkCopy}>
+                    {linkHasCopied ? "Copied!" : "Copy"}
+                  </Button>
+                </Flex>
+                <Code padding="4" variant="subtle" colorScheme="green">
+                  {linkValue}
+                </Code>
+                <Flex justifyContent="space-between" paddingY="4">
+                  <Tag>HTML</Tag>
+                  <Button size="xs" onClick={onHtmlCopy}>
+                    {htmlHasCopied ? "Copied!" : "Copy"}
+                  </Button>
+                </Flex>
+                <Code padding="4" variant="subtle" colorScheme="blue">
+                  {HtmlValue}
+                </Code>
+              </Flex>
+            </Flex>
+          )}
+        </Box>
+        <Box marginTop="5" marginBottom="20">
+          <Text textAlign="justify">
+            This tool was created by , it is provided free of charge and without
+            warranty. If you find it useful, please consider{" "}
+            <Button
+              variant="link"
+              onClick={() => {
+                // open the link in a new tab
+                window.open("https://www.buymeacoffee.com/suarezluis");
+              }}
+            >
+              buying me a coffee
+            </Button>
+            . If you have any questions, comments, or suggestions, please{" "}
+            <Button
+              variant="link"
+              onClick={() => {
+                // open the link in a new tab
+                window.open("https://x.com/suarezluis");
+              }}
+            >
+              reach out to me on X (formerly Twitter)
+            </Button>
+            .
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Flex>
   );
 }
 
