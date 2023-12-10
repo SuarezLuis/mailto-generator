38ae7b8d9b7ee74fea81488e1651706deca925aa
Luis Suarez <suarezluis@gmail.com>
2023-12-09 23:31:13 -0600

    Add github sponsor

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -29,6 +29,16 @@ const Footer = (props: Props) => {
         >
           buying me a coffee
         </Button>
+        , or sponsoring me on{" "}
+        <Button
+          variant="link"
+          onClick={() => {
+            // open the link in a new tab
+            window.open("https://github.com/sponsors/suarezluis");
+          }}
+        >
+          GitHub
+        </Button>
         . If you have any questions, comments, or suggestions, please{" "}
         <Button
           variant="link"
